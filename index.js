6ffd27c2b0b01704d9ce2633d1867a85b8a428be
ivoshet <ivan.v.shet@gmail.com>
2019-05-30 23:15:02 +0500

    all works

--- index.js
+++ index.js
@@ -2,28 +2,50 @@
 //to need connect mkdir package npm i mkdirp --save
 var mkdirp = require('mkdirp');
 
-//
+//the pathes to folders
+var public_folder = '/public';
+var controllers_folder = '/controllers';
+var helpers_folder = '/helpers';
+var server_folder = '/server';
+var views_folder = '/views';
 
-
-
-
-
+//nested folder
+var public_css = '/public/css';
+var public_img = '/public/img';
+var public_js = '/public/js';
+var public_upload = '/public/upload';
+var public_upload_temp = '/public/upload/temp';
+var views_layouts = '/views/layouts';
+var views_partials = '/views/partials';
 
 //detect current name
 var current_folder = __dirname;
 
-const create_folder_one = (name_folder) => {
-    var full_name = String(name_folder) + '/ops'
+const create_folder = (name_folder) => {
+    var full_name = current_folder + name_folder;
     mkdirp(full_name, (err) => {
 	if(err) console.log(err);
 	else console.log('pow')
     })
 }
 
-const output_name = (name) => {
-    console.log(name);
-}
+/* const output_name = (name) => {
+ *     console.log(name);
+ * }*/
 
-/* output_name(current_folder);*/
-create_folder_one(__dirname);
+//create folders
+create_folder(public_folder);
+create_folder(controllers_folder);
+create_folder(helpers_folder);
+create_folder(server_folder);
+create_folder(views_folder);
+
+//create nested folders
+create_folder(public_css);
+create_folder(public_img);
+create_folder(public_js);
+create_folder(public_upload);
+create_folder(public_upload_temp);
+create_folder(views_layouts);
+create_folder(views_partials);
 
