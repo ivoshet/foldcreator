576eec4893c06f8090000a3b075e3a84fc12394f
ivoshet <ivan.v.shet@gmail.com>
2019-05-31 00:32:14 +0500

    created success message

--- index.js
+++ index.js
@@ -7,15 +7,38 @@ var list = require('./list_folders');
 //detect current name
 var current_folder = __dirname;
 
+//the counter for understandig when created project structure
+var counter = 0;
+
 const create_folder = (name_folder) => {
     var full_name = current_folder + name_folder;
+    
+    /* because mkdirp callback work after end loops 
+       to need output status before mkdirp */
+    
+    console.log(`create : ${name_folder}`);
+
+    //call mkdirp
     mkdirp(full_name, (err) => {
 	if(err) console.log(err);
-	else console.log('pow')
     })
 }
 
-for(var i = 0; i < list.folder_list.length; i++) {
-    create_folder(list.folder_list[i]);
+//the main loop
+const create_folder_structure = () => {
+    for(var i = 0; i < list.folder_list.length; i ++) {
+	create_folder(list.folder_list[i]);
+	counter ++;
+    };
 }
+
+//output that structure created
+const output_success = () => {
+    if(counter == list.folder_list.length){
+	console.log('\n' + 'project structure created success'); 
+    }
+}
+
+create_folder_structure();
+output_success();
 
