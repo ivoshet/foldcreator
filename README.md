ed0be62fa12cff80a2d2eb7ba87c6920ae183c93
ivoshet <ivan.v.shet@gmail.com>
2019-05-31 12:36:11 +0500

    added move function

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-this script needs for creating of express folder structure. I hope it is good instrument.
+/*
+this instrument for create simple express project environments.
+I hope that it will be usefull for you
+*/
+
+npm i create_structure -g
+
+//then start in your project directory
+
+create_structure
---
+++ templates_setting.js
@@ -0,0 +1,13 @@
+/* add template files into templates directory and
+   add file's name in array templates_docs
+   file.txt and file_1.txt are testing file
+ */
+const templates_docs = [
+    '.editorconfig',
+    '.gitignore',
+    'README.md',
+    'file.txt',
+    'file_1.txt'
+];
+
+module.exports = {templates_docs};
